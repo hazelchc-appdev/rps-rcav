397969c320c9aafd5f9d2ef1e7ae884277f0cee4
Hazel Chui <hazelchui@uchicago.edu>
2023-04-09 17:26:51 +0000

    halfway

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+
+
+    # redirect_to("https://google.com")
+    # render({ :plain => rand(100)})
+    # render({ :html => "<h1> Hello </h1>".html_safe})
+    render({ :template => "game_templates/user_rock.html.erb"})
+
+  end 
 end
