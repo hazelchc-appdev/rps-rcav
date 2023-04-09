771540ab061cc7f98a800121b13ae75d4abea753
Hazel Chui <hazelchui@uchicago.edu>
2023-04-09 18:16:38 +0000

    Lots of progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
   def play_rock
 
 
@@ -11,6 +15,23 @@ class ApplicationController < ActionController::Base
     # render({ :plain => rand(100)})
     # render({ :html => "<h1> Hello </h1>".html_safe})
     render({ :template => "game_templates/user_rock.html.erb"})
-
   end 
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render ({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "applicatio", :action => "homepage"})
+
   get("/rock", {:controller => "application", :action => "play_rock"})
+
+  get("/paper", {:controller => "application", :action => "play_paper"})
 
 end
